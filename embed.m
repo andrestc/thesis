9a7570a9b0fa363a7d64050a8522d1e00a9bb8cc
André Carvalho <asantostc@gmail.com>
2016-10-29 10:50:31 -0200

    remove unused embed func

--- embed.m
+++
@@ -1,7 +0,0 @@
-function RPS = embed(x,dim,tau)
-    % phase space reconstruction using time delay embedding
-    RPS=zeros(T,dim);
-    for i=1:N-(dim-1)*tau
-       RPS(i,:)=x(i+(0:dim-1)*tau)';
-    end
-end
