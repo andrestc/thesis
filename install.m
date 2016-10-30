55df48f8eb04032ad11d766e92f64e2c33e61f01
André Carvalho <asantostc@gmail.com>
2016-10-30 11:53:58 -0200

    install: create results dir if it does not exists

--- install.m
+++ install.m
@@ -1,2 +1,8 @@
 addpath(fullfile(pwd, 'deps'),path);
 addpath(fullfile(pwd,'funcs'),path);
+
+resultsDir = sprintf('./results');
+% Finally, create the folder if it doesn't exist already.
+if ~exist(resultsDir, 'dir')
+  mkdir(resultsDir);
+end
