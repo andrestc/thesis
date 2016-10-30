d59ccb1e0c87f7e49dc54f4dccb530816ef7cc5b
André Carvalho <asantostc@gmail.com>
2016-10-30 10:25:49 -0200

    fix experiment script

--- class_exp.m
+++ class_exp.m
@@ -1,12 +1,12 @@
 clear; clc;
 
 SETS = {'Synthetic_Control' 'Gun_Point' 'CBF' 'Trace' 'FaceFour' 'Lighting2' 'Lighting7' 'ECG200' 'Beef' 'Coffee' 'OliveOil'};
-%SETS = {'Synthetic_Control'};
+
 for s=SETS
     set = s{:};
     disp(set);
-    TRAIN = load(strcat('../UCR_TS_Archive_2015/',set,'/',set,'_TRAIN'));
-    TEST = load(strcat('../UCR_TS_Archive_2015/',set,'/',set,'_TEST'));
+    TRAIN = load(strcat('data/',set,'/',set,'_TRAIN'));
+    TEST = load(strcat('data/',set,'/',set,'_TEST'));
     
     tau = choose_tau(TRAIN(:,2:end)); 
     d = choose_dimension(TRAIN(:,2:end), tau);
@@ -52,7 +52,7 @@ writetable(T, 'table.csv')
 
 
 
-%% Multiple d, tau e h (Força Bruta)
+%% Multiple d, tau e h (For?a Bruta)
 clear; clc;
 
 %SETS = {'Synthetic_Control' 'Gun_Point' 'CBF' 'Trace' 'FaceFour' 'Lighting2' 'Lighting7' 'ECG200' 'Beef' 'Coffee' 'OliveOil'};
@@ -65,8 +65,8 @@ h = [0.1];
 for s=SETS
     set = s{:};
     disp(set);
-    TRAIN = load(strcat('../UCR_TS_Archive_2015/',set,'/',set,'_TRAIN'));
-    TEST = load(strcat('../UCR_TS_Archive_2015/',set,'/',set,'_TEST'));
+    TRAIN = load(strcat('data/',set,'/',set,'_TRAIN'));
+    TEST = load(strcat('data/',set,'/',set,'_TEST'));
         
     acc = []; conf = {}; dists = {}; elapsed = [];
     for i=1:length(h)
@@ -90,8 +90,7 @@ for s=SETS
 end
 
 %% Genereta results table
-%SETS = {'Synthetic_Control' 'Gun_Point' 'CBF' 'Trace' 'FaceFour' 'Lighting2' 'Lighting7' 'ECG200' 'Beef' 'Coffee' 'OliveOil'};
-SETS = { 'Lighting2' 'Lighting7' 'Beef' 'OliveOil'};
+SETS = {'Synthetic_Control' 'Gun_Point' 'CBF' 'Trace' 'FaceFour' 'Lighting2' 'Lighting7' 'ECG200' 'Beef' 'Coffee' 'OliveOil'};
 results = zeros(4,4);
 l = 1;
 for s=SETS
@@ -109,151 +108,3 @@ disp(T);
 
 
 
-%% Primeiro EXP agora com GMM
-clear; clc;
-
-SETS = {'Synthetic_Control' 'Gun_Point' 'CBF' 'Trace' 'FaceFour' 'Lighting2' 'Lighting7' 'ECG200' 'Beef' 'Coffee' 'OliveOil'};
-
-for s=SETS
-    set = s{:};
-    disp(set);
-    TRAIN = load(strcat('../UCR_TS_Archive_2015/',set,'/',set,'_TRAIN'));
-    TEST = load(strcat('../UCR_TS_Archive_2015/',set,'/',set,'_TEST'));
-    
-    tau = choose_tau(TRAIN(:,2:end)); 
-    d = choose_dimension(TRAIN(:,2:end), tau);
-    h = [1 2 4 8 16 32];
-    
-    acc = []; conf = {}; dists = {};
-    while length(acc) < length(h)
-        i = length(acc) + 1;
-        class = zeros(size(TEST,1), 1);
-        D = zeros(size(TEST,1), size(TRAIN,1));
-        tic;
-        try
-            models = learnModelsMat(TRAIN, h(i), tau, d);
-        catch
-            disp(['failed to learn models', num2str(i)]);
-            continue
-        end
-        for j=1:size(TEST,1)
-            class(j) = classify(TEST(j,2:end),models, tau, d);
-        end
-        elapsed(i) = toc;
-        acc(i) = mean(class == TEST(:,1));
-        conf{i} = confusionmat(TEST(:,1), class);
-        disp([h(i) acc(i)]);
-        save(strcat('./results/', set, '_gmm.mat'), 'acc', 'conf', 'd', 'h', 'tau', 'i', 'set', 'elapsed');
-    end
-end
-
-%% Generate results table
-SETS = {'Synthetic_Control' 'Gun_Point' 'CBF' 'Trace' 'FaceFour' 'Lighting2' 'Lighting7' 'ECG200' 'Beef' 'Coffee' 'OliveOil'};
-
-results = [];
-for s=SETS
-    set = s{:};
-    r = load(strcat('./results/', set, '_gmm.mat'));
-    results = [results; r.acc];
-end
-
-header = {};
-i = 1;
-for h=r.h
-    header{i} = strcat('m_', strrep(num2str(h), '.', '_'));
-    i = i + 1;
-end
-T = array2table(results, 'RowNames', SETS, 'VariableNames', header);
-disp(T);
-
-writetable(T, 'table_gmm.csv')
-
-
-%% Multiple d, tau e h (Força Bruta)
-clear; clc;
-
-SETS = {'Synthetic_Control' 'Gun_Point' 'CBF' 'Trace' 'FaceFour' 'Lighting2' 'Lighting7' 'ECG200' 'Beef' 'Coffee' 'OliveOil'};
-
-tau = [1 3 5];
-d = 2:2:20;
-h = [8 16 32];
-    
-for s=SETS
-    set = s{:};
-    disp(set);
-    TRAIN = load(strcat('../UCR_TS_Archive_2015/',set,'/',set,'_TRAIN'));
-    TEST = load(strcat('../UCR_TS_Archive_2015/',set,'/',set,'_TEST'));
-        
-    acc = []; conf = {}; dists = {};
-    for i=1:length(h)
-        for j=1:length(d)
-            for k=1:length(tau)
-                class = zeros(size(TEST,1), 1);
-                D = zeros(size(TEST,1), size(TRAIN,1));
-                try
-                    models = learnModelsMat(TRAIN, h(i), tau(k), d(j));
-                catch
-                    disp('failed to learn models');
-                    acc(i,j,k) = 0;
-                    conf{i,j,k} = [];
-                    save(strcat('./results/FB_', set, '_gmm.mat'), 'acc', 'conf', 'd', 'h', 'tau', 'i', 'set');
-                    continue
-                end
-                for x=1:size(TEST,1)
-                    try
-                        class(x) = classify(TEST(x,2:end),models, tau(k), d(j));
-                    catch
-                        class(x) = Inf;
-                    end
-                end
-
-                acc(i,j,k) = mean(class == TEST(:,1));
-                conf{i,j,k} = confusionmat(TEST(:,1), class);
-                disp([h(i) acc(i,j,k) d(j) tau(k)]);
-                save(strcat('./results/FB_', set, '_gmm.mat'), 'acc', 'conf', 'dists', 'd', 'h', 'tau', 'i', 'set');
-            end
-            
-        end
-
-    end
-end
-
-%% Genereta results table
-SETS = {'Synthetic_Control' 'Gun_Point' 'CBF' 'Trace' 'FaceFour' 'Lighting2' 'Lighting7' 'ECG200' 'Beef' 'Coffee' 'OliveOil'};
-
-results = zeros(11,4);
-tau = [1 3 5];
-d = 2:2:20;
-h = [8 16 32];
-l = 1;
-for s=SETS
-    set = s{:};
-    r = load(strcat('./results/FB_', set, '_gmm.mat'));
-    acc = r.acc(:,:,1);
-    [m, I] = max(acc(:));
-    [i, j, k] = ind2sub(size(r.acc),I);
-    results(l,:) = [m r.h(i) r.d(j) r.tau(k)];
-    l=l+1;
-end
-T = array2table(results, 'RowNames', SETS, 'VariableNames', {'acc' 'h' 'd' 'tau'});
-disp(T);
-
-writetable(T, 'tableFB_gmm.csv')
-
-
-%% view results elapsed time
-clear; clc;
-SETS = {'Synthetic_Control' 'Gun_Point' 'CBF' 'Trace' 'FaceFour' 'Lighting2' 'Lighting7' 'ECG200' 'Beef' 'Coffee' 'OliveOil'};
-Eise = [];
-Egmm = [];
-Mgmm = [];
-for s=SETS
-    set = s{:};
-    r = load(strcat('./results/', set, '.mat'));
-    Eise = [Eise; r.elapsed];
-    Hise = [r.h];
-    
-    r = load(strcat('./results/', set, '_gmm.mat'));
-    Egmm = [Egmm; r.elapsed];
-    Mgmm = [r.h];
-end
